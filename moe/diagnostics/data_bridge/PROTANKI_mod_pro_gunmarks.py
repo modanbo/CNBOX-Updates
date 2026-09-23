bd1b21b9dd295470f878aa37326bf51f9f3bcb13
modanbo <modanbo@gmail.com>
2026-09-23 15:54:16 -0400

    Remove one-shot/protected MoE bridge analysis artifact

--- moe/diagnostics/data_bridge/PROTANKI_mod_pro_gunmarks.py
+++
@@ -1,5 +0,0 @@
-# uncompyle6 version 3.9.3
-# Python bytecode version base 2.7 (62211)
-# Decompiled from: Python 3.12.3 (main, Aug 31 2026, 10:18:26) [GCC 13.3.0]
-# Embedded file name: mod_pro_gunmarks
-# Compiled at: 2026-08-31 21:24:49
