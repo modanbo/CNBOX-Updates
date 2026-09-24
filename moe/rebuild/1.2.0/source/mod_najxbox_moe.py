ee5a2cc447fbfe7574a8d16b78a5ce12251b5a24
modanbo <modanbo@gmail.com>
2026-09-23 20:49:32 -0400

    MoE 1.2.0: close WoT gui.mods package imports

--- moe/rebuild/1.2.0/source/mod_najxbox_moe.py
+++ moe/rebuild/1.2.0/source/mod_najxbox_moe.py
@@ -2,7 +2,7 @@
 """World of Tanks loader entry for NAJXBox MoE 1.2.0."""
 
 try:
-    from najxbox_moe import view
+    from gui.mods.najxbox_moe import view
     view.setup()
 except Exception:
     # Never break client startup because of the optional MoE component.
